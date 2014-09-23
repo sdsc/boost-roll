63a5ffa2649f7350e26969558a7cdfda679e8e3c
Jim Hayes <jhayes@sdsc.edu>
2014-09-23 16:02:33 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/boost-modules/version.mk
+++ src/boost-modules/version.mk
@@ -3,8 +3,12 @@ ifndef ROLLCOMPILER
 endif
 COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
-NAME       = boost-modules_$(COMPILERNAME)
-VERSION    = 1.55.0
-RELEASE    = 0
+NAME        = boost-modules_$(COMPILERNAME)
+RELEASE     = 1
+PKGROOT     = /opt/modulefiles/applications/.$(COMPILERNAME)/boost
 
-RPM.EXTRAS = "AutoReq: no"
+VERSION_SRC = $(REDHAT.ROOT)/src/boost/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
